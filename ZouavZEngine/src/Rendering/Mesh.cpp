90b36a7efebc72af258ba0f4d7095cef6a41604f
Jeremy97 <j.grondin@student.isartdigital.com>
2021-05-21 14:21:18 +0200

    change bind bone data to shader

--- ZouavZEngine/src/Rendering/Mesh.cpp
+++ ZouavZEngine/src/Rendering/Mesh.cpp
@@ -140,11 +140,11 @@ void Mesh::InitMesh(Vertex* vertices, size_t vertSize, int* indices, size_t indi
 	
 	//For animation
 	// ids
-	glVertexAttribIPointer(3, 4, GL_INT, sizeof(Vertex), (const GLvoid*)0);
+	glVertexAttribIPointer(3, 4, GL_INT, sizeof(Vertex), (const GLvoid*)(9 * sizeof(int)));
 	glEnableVertexAttribArray(3);
 
 	// weights
-	glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)0);
+	glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const GLvoid*)(const GLvoid*)(13 * sizeof(float)));
 	glEnableVertexAttribArray(4);
 }
 
